b3d7e1117b986bd0b73fdc88f325f50d0cab78b3
JuanMa <64336592+Juanmacha@users.noreply.github.com>
2025-08-28 09:59:54 -0500

    Update main.py

--- regresiones_lineales/main.py
+++ regresiones_lineales/main.py
@@ -1,19 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import pandas as pd
 import joblib
 
-# 1. cargar el modelo y las columnas
+# cargar modelo y columnas
 model = joblib.load('modelo_regresion_lineal.pkl')
 columnas = joblib.load('columnas_modelo.pkl')
 
 app = FastAPI(title="Predicción de Precios de Casas")
+
+class HouseData(BaseModel):
+    size: float
+    bedrooms: int
+    age: int
 
 @app.get("/")
 def home():
     return {"message": "Bienvenido a la API de Predicción de Precios de Casas"}
 
 @app.post("/predict")
-def predict(size: float, bedrooms: int, age: int):
-    x_new = pd.DataFrame([[size, bedrooms, age]], columns=columnas)
+def predict(data: HouseData):
+    x_new = pd.DataFrame([[data.size, data.bedrooms, data.age]], columns=columnas)
     prediction = model.predict(x_new)
-    return {"predicted_price": prediction[0]}
+    return {"Prediccion de precio": prediction[0]}
+
